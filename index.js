1e02bc4a48be14b3289620d296d50a6601df6467
Zachary Harris <the1zachharris@gmail.com>
2024-02-01 14:15:55 -0600

    pathing and nav bar works!

--- index.js
+++ index.js
@@ -14,6 +14,22 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/bio", (req, res) => {
+  res.render("bio");
+});
+
+app.get("/experience", (req, res) => {
+  res.render("experience");
+});
+
+app.get("/portfolio", (req, res) => {
+  res.render("port");
+});
+
+app.get("*", (req, res) => {
+  res.render("error");
+});
+
 app.listen(3000, () => {
   console.log("Serving on port 3000!");
 });
