ab9f6ed3d6cdd742b3b4eb9023f0a477a47e62c0
Zachary Harris <the1zachharris@gmail.com>
2024-02-01 13:07:06 -0600

    trying to get templates to work

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ import express from 'express';
 
 const app = express()
 
+app.engine('ejs', engine);
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs'); // so you can render('index')
+
+// render 'index' into 'boilerplate':
+app.get('/',function(req,res,next){
+  res.render('index');
+});
 
 
 
